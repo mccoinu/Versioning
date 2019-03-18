ac804895fcc2fdcd10969fffe7fd058142c0f5ee
begear <begear@DESKTOP-5OF30VP.station>
2019-03-18 10:17:14 +0100

    Inseriti costruttore e metodi getter & setter

--- src/Prova1.java
+++ src/Prova1.java
@@ -6,4 +6,44 @@ public class Prova1 {
 	private String s;
 	private boolean check;
 	
+	public Prova1(int x, int y, String s, boolean check) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.s = s;
+		this.check = check;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getS() {
+		return s;
+	}
+
+	public void setS(String s) {
+		this.s = s;
+	}
+
+	public boolean isCheck() {
+		return check;
+	}
+
+	public void setCheck(boolean check) {
+		this.check = check;
+	}
+	
 }
